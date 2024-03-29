e4e86902c9a532546c1abf43cbbc060d27402191
Debmalya Ray <66347154+DebmalyaRay9989@users.noreply.github.com>
2024-03-29 20:15:05 +0530

    .
    .

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+
 from flask import Flask, render_template, request
 import jsonify
 import requests
@@ -6,6 +7,7 @@ import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
 model = pickle.load(open('random.pkl', 'rb'))
 @app.route('/',methods=['GET'])
@@ -20,23 +22,23 @@ def predict():
     prediction=0
     if request.method == 'POST':
 	source_port = int(request.form['source_port'])
-        destination_port = int(request.form['destination_port'])
-        nat_source_port = int(request.form['nat_source_port'])
-        nat_destination_port = int(request.form['nat_destination_port'])
-        bytes1 = int(request.form['bytes1'])
+    destination_port = int(request.form['destination_port'])
+    nat_source_port = int(request.form['nat_source_port'])
+    nat_destination_port = int(request.form['nat_destination_port'])
+    bytes1 = int(request.form['bytes1'])
 	bytes_sent = int(request.form['bytes_sent'])	
 	bytes_received = int(request.form['bytes_received'])
 	packets = int(request.form['packets'])
 	elapsed_time_sec = int(request.form['elapsed_time_sec'])
 	pkts_sent = int(request.int['pkts_sent'])
-        pkts_received = int(request.form['pkts_received'])
+    pkts_received = int(request.form['pkts_received'])
 
-        prediction=model.predict([[source_port,destination_port,nat_source_port,nat_destination_port,bytes1,bytes_sent,bytes_received,packets,elapsed_time_sec,pkts_sent,pkts_received]])
-        output=int(prediction)
+    prediction=model.predict([[source_port,destination_port,nat_source_port,nat_destination_port,bytes1,bytes_sent,bytes_received,packets,elapsed_time_sec,pkts_sent,pkts_received]])
+    output=int(prediction)
 
-        if output == 0:
+    if output == 0:
 	    return render_template('results.html',prediction_text="allow")
-        if output == 1:
+    if output == 1:
 	    return render_template('results.html',prediction_text="deny")
 	if output == 2:
 	    return render_template('results.html',prediction_text="drop")
@@ -50,3 +52,7 @@ if __name__=="__main__":
     app.run(debug=True)
 	
 	
+	
+
+
+
