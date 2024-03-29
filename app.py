275e233b33d9850ba3371bfdd54e0e773666fd0c
Debmalya Ray <66347154+DebmalyaRay9989@users.noreply.github.com>
2024-03-29 20:38:01 +0530

    Update app.py
    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-model = pickle.load(open('random.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/',methods=['GET'])
